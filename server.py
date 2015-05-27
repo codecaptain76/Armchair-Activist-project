b82d7c9e9b60ff887a4ea42a8d59e4d3b78dcf39
Keirra Hall <keirrahall@Keirras-MacBook-Pro.local>
2015-05-27 14:20:05 -0700

    fixed 400 error, in process of getting search results to pprint to browser from json

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session as browser_session
 #from flask_debugtoolbar import DebugToolbarExtension
 from secrets import amm_chain
+import pprint
 
 import requests
 
@@ -45,54 +46,99 @@ def signin():
 @app.route("/search", methods=['GET'])
 def search():
 
-	if 'keyword' in request.args:
+	# if 'keyword' in request.args:
 		
-		keyword = request.args['keyword']
-		#if submitting form, go to API to do search
-		search= requests.get(amm_search_url+'&keyword='+keyword)
-		print search.json()['results']
+	# 	keyword = request.args['keyword']
+	# 	#if submitting form, go to API to do search
+	# 	search= requests.get(amm_search_url+'&keyword='+keyword)
+	# 	# pprint.pprint(search.json())
+	# 	results = search.json()['results']
+	# 	pprint.pprint(results)
+
+	# 	filtered_results = []		
+	# 	for key in results:
+	# 		print results[key]['beneficiaryName']
+	# 		print results[key]['beneficiaryId']
+	# 		print results[key]['strapline']
+	# 		if 'country' in results[key]:
+	# 			if results[key]['country'] == 'US':
+	# 				print results[key]['country']
+	# 		print results[key]['beneficiaryType']
+	# 		filtered_results.append(results[key])
+
+	# 	pprint.pprint(filtered_results)
+
+	# 	return render_template('results.html', results=filtered_results)
+
+
 		#make a dictionary of {beneficiary name: benefic_id, ... }
 		#iterate through list, add to my dictionary
 
-		beneficiaryName = request.args["beneficiaryName"]
-		beneficiaryId = request.args["beneficiaryId"]
-		strapline = request.args["strapline"]
-		country = request.args["country"]
-		beneficiaryType = request.args["beneficiaryType"]
+		# beneficiaryName = request.args["beneficiaryName"]
+		# beneficiaryId = request.args["beneficiaryId"]
+		# strapline = request.args["strapline"]
+		# country = request.args["country"]
+		# beneficiaryType = request.args["beneficiaryType"]
 		
 
-	search_info = {'beneficiaryName' : 'beneficiaryId', 
-						'strapline' : [],
-	                    'country' : "US",
-						'beneficiaryType' : 'nonprofit'}
+	# search_info = {'beneficiaryName' : 'beneficiaryId', 
+	# 					'strapline' : [],
+	#                     'country' : "US",
+	# 					'beneficiaryType' : 'nonprofit'}
 
-	
+	# for keyword in search_info:
 
 
-	#my_dict = []
+	#my_dict = {}
 		
 	#for keyword in search_info:
   	#		my_dict.append(keyword)
 
 
   	#		return my_dict
-  	print search_info
+  	# print search_info
 
 	return render_template("search.html")
 
-  	for k in search.iteritems():
+  	# for k in search.iteritems():
 
 
-			return render_template('results.html', search=search.json(),
-											   beneficiaryName=beneficiaryName,
-										       strapline=strapline,
-										       country=country,
-										       beneficiaryType=beneficiaryType
-										       )
-			#print "Search results are:"('%s, %s, %s, %s,') % (search[beneficiaryName], [strapline],
-			#[country], [beneficiaryType])
+			# return render_template('results.html', search=search.json(),
+			# 								   beneficiaryName=beneficiaryName,
+			# 							       strapline=strapline,
+			# 							       country=country,
+			# 							       beneficiaryType=beneficiaryType
+			# 							       )
+			# print "Search results are:"('%s, %s, %s, %s,') % (search[beneficiaryName], [strapline],
+			# [country], [beneficiaryType])
 
 	
+@app.route("/results")
+def results():
+	if 'keyword' in request.args:
+		
+		keyword = request.args['keyword']
+		#if submitting form, go to API to do search
+		search= requests.get(amm_search_url+'&keyword='+keyword)
+		# pprint.pprint(search.json())
+		results = search.json()['results']
+		# pprint.pprint(results)
+
+		filtered_results = []		
+		for key in results:
+			print str(results[key]['beneficiaryName'])
+			print str(results[key]['beneficiaryId'])
+			print str(results[key]['strapline'])
+			if 'country' in results[key]:
+				if results[key]['country'] == 'US':
+					print str(results[key]['country'])
+			if results[key]['beneficiaryType'] == 'nonprofit':
+				print str(results[key]['beneficiaryType'])
+			filtered_results.append(results[key])
+
+		# pprint.pprint(filtered_results)
+
+		return render_template('results.html', results=filtered_results)
 
 
 @app.route("/donate", methods=['GET'])
